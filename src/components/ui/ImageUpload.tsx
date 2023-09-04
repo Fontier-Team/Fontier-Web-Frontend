e74a9aa31ad099653dc01c587c25688fe5c4b7c6
RaymondWHZ <wuhaozhen1106@gmail.com>
2023-09-04 10:18:31 -0500

    feat: add image upload component

---
+++ src/components/ui/ImageUpload.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {useDropzone} from "react-dropzone";
+import Image from "next/image";
+
+interface ImageUploadZoneProps {
+  image: File | null
+  onUpload: (image: File) => void
+}
+
+export const ImageUploadZone: React.FC<ImageUploadZoneProps> = ({ onUpload, image }) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      onUpload(acceptedFiles[0])
+    }
+  })
+
+  return (
+    <div
+      {...getRootProps()}
+      className="gray-card relative h-full w-full flex flex-col items-center justify-center text-primary-gray cursor-pointer text-lg font-light gap-6"
+    >
+      <input {...getInputProps()} />
+      <Image src="/camera.svg" alt="Upload Icon" width={50} height={50} />
+      {isDragActive ? (
+        <>Drop image here</>
+      ) : image ? (
+        <Image src={URL.createObjectURL(image)} alt="Uploaded Image" layout="fill" objectFit="contain" />
+      ) : (
+        <>Click or drag and drop to upload</>
+      )}
+    </div>
+  )
+}
